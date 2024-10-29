b16da5bd2deab02c0d8d857c6a77413b885ddef5
gui <guilhermev2huehue@gmail.com>
2024-10-29 20:44:18 -0300

    feat: reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
     let file_path = &args[2];
+    
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
 
-    println!("searching for {query}");
-    println!("in file {file_path}");
-
+    println!("with text: \n{contents}");
 }
